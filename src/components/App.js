ad6d5ef3e11aed99cc7e2b9153d62b9bac75c6ac
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-08-06 21:46:51 +0000

    Fixed react simple data fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+// create your App component 
+import React, { useEffect, useState }from "react";
+
+const App = () => {
+    const [dogData, setDogData] = useState(null)
+
+    useEffect(()=> {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(response => response.json())
+            .then(data => setDogData(data.message))
+    }, [])
+    
+    //const listDogData = dogData.map(dog => <img src={dog.message} alt="A Random Dog"/>)
+    if (!dogData) {
+    return <p>Loading...</p> 
+    }
+    return (
+        <img src={dogData} alt="A Random Dog"/>
+    ) 
+}
+export default App;
+
